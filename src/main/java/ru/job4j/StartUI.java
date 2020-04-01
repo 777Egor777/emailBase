81ca8e86b88079b4480eab52db91992601d345bd
Egor <yegeraskin13@gmail.com>
2020-04-01 16:25:29 +0400

    Add first JavaDoc for the StartUI class(to be extended)

--- src/main/java/ru/job4j/StartUI.java
+++ src/main/java/ru/job4j/StartUI.java
@@ -1,5 +1,15 @@
 package ru.job4j;
 
+/**
+ * Start User Interface
+ * We use this class to start
+ * interact with user by Console
+ * input - output.
+ * //TODO write about methods of the class
+ *
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ * @version 0.1, 01.04.2020
+ */
 public class StartUI {
     public static void main(String[] args) {
     }
