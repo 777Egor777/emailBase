0060881c12f05686050934a8842b70520c7dfbeb
Egor <yegeraskin13@gmail.com>
2020-04-02 22:48:13 +0400

    Add JavaDoc for the class "Data"

--- src/main/java/ru/job4j/Data.java
+++ src/main/java/ru/job4j/Data.java
@@ -1,4 +1,18 @@
 package ru.job4j;
 
+/**
+ * Data model of the
+ * user's email database.
+ * For each user we keep
+ * their unique email-list
+ *
+ * We can also "compress"
+ * this database: join users
+ * that have at least one
+ * common email in their
+ * lists.
+ *
+ * @author Egor Geraskin(yegeraskin13@gmail.com)
+ */
 public class Data {
 }
