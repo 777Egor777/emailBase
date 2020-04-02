552fb31c51d1d1495a8634e7852ede0c360f8a26
Egor <yegeraskin13@gmail.com>
2020-04-02 21:07:09 +0400

    Add JavaDoc for the "Input" interface

--- src/main/java/ru/job4j/Input.java
+++ src/main/java/ru/job4j/Input.java
@@ -1,4 +1,23 @@
 package ru.job4j;
-
+/**
+ * Interface that describes
+ * input operations.
+ * In this program we need
+ * to get follow types of data:
+ * String, number, e-mail,
+ * User model
+ *
+ * This interface - part of
+ * pattern "Strategy".
+ * We use this pattern to
+ * organise input-output
+ * in the program,
+ * and test it.
+ *
+ * We will make some
+ * implementaions of it.
+ *
+ * @author Geraskin Egor(yegeraskin13@gmail.com)
+ */
 public interface Input {
 }
