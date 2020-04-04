84ba876b86a04dc769ddc533c473d7f9e933b4f7
Egor <yegeraskin13@gmail.com>
2020-04-04 11:47:32 +0400

    Add package sub-package model in main package Also add "package-info" file to it

---
+++ src/main/java/ru/job4j/model/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * In this package ve save all
+ * data models in this program.
+ * Each model can be in sub-package
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ */
+package ru.job4j.model;
