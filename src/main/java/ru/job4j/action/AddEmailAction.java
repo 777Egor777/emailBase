3a2bfd63c4403cd4fefaceef48c49938377c2e05
Egor <yegeraskin13@gmail.com>
2020-04-04 20:41:24 +0400

    Add class "AddEmailAction" implements "UserAction" interface. Also add frames for methods getTitle() and execute()

---
+++ src/main/java/ru/job4j/action/AddEmailAction.java
@@ -0,0 +1,44 @@
+package ru.job4j.action;
+
+import ru.job4j.input.Input;
+import ru.job4j.model.Data;
+
+public class AddEmailAction implements UserAction {
+    /**
+     * Method give name of
+     * this action.
+     * This name need
+     * during printing menu
+     *
+     * @return short name of this
+     * action.
+     */
+    @Override
+    public String getTitle() {
+        return null;
+    }
+
+    /**
+     * Inside this method
+     * performs all needed
+     * operations on this
+     * type of user's action
+     *
+     * @param input - instance of object,
+     *              that we use to input
+     *              data into program
+     * @param data  - instance of Data
+     *              object, that we will
+     *              make operations with
+     * @return boolean value that
+     * reports, should
+     * program stop or
+     * continue work-
+     * cycle after this
+     * action.
+     */
+    @Override
+    public boolean execute(Input input, Data data) {
+        return false;
+    }
+}
