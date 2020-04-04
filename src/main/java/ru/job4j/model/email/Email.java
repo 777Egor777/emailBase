a475a23ff750fb377f755d5b0af29028f2b4d9c9
Egor <yegeraskin13@gmail.com>
2020-04-04 21:46:44 +0400

    Make class "Email" final

--- src/main/java/ru/job4j/model/email/Email.java
+++ src/main/java/ru/job4j/model/email/Email.java
@@ -7,7 +7,7 @@ package ru.job4j.model.email;
  *
  * @author Egor Geraskin(yegeraskin13@gmail.com)
  */
-public class Email {
+public final class Email {
     /**
      * String value of email
      * For example: "yegeraskin13@gmail.com"
