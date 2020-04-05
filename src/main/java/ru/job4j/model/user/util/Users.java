e2299138d32bcfc2c06aef31b105e70243a6d491
Egor <yegeraskin13@gmail.com>
2020-04-05 14:35:56 +0400

    Add JavaDoc for class "Users"

--- src/main/java/ru/job4j/model/user/util/Users.java
+++ src/main/java/ru/job4j/model/user/util/Users.java
@@ -1,4 +1,14 @@
 package ru.job4j.model.user.util;
 
+/**
+ * Help utility class for
+ * data model "User".
+ *
+ * It will be very helpful when
+ * we decide to make this class
+ * Immutable
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ */
 public class Users {
+
 }
