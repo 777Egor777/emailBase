37890b7f9125e86253f04c9968c68eeee114254a
Egor <yegeraskin13@gmail.com>
2020-04-05 15:54:09 +0400

    Change JavaDoc in class "User"

--- src/main/java/ru/job4j/model/user/User.java
+++ src/main/java/ru/job4j/model/user/User.java
@@ -1,10 +1,15 @@
 package ru.job4j.model.user;
 
+import ru.job4j.model.email.Email;
+import java.util.List;
+
 /**
  * Data model for the
  * single user that
  * base consists from
  *
+ * We can make this class
+ * Immutable in future
  * @author Geraskin Yegor(yegeraskin13@gmail.com)
  */
 public class User {
