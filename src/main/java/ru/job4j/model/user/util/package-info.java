042dbf3ce9e4b16997cc03360eddfe274540260b
Egor <yegeraskin13@gmail.com>
2020-04-05 13:26:50 +0400

    Add sub-package "util" in package "user". And "package-info" file for it

---
+++ src/main/java/ru/job4j/model/user/util/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package include classes, that
+ * performs utility functions
+ * for {@code User} class
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ */
+package ru.job4j.model.user.util;
