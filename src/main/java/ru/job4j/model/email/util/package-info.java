3ce3282bdf0342fad47432139b93742d6dc597d4
Egor <yegeraskin13@gmail.com>
2020-04-05 13:00:52 +0400

    Add sub-package "util" in package "email" And add "package-info" file for it

---
+++ src/main/java/ru/job4j/model/email/util/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package include classes, that
+ * performs utility functions
+ * for {@code Email} class
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ */
+package ru.job4j.model.email.util;
