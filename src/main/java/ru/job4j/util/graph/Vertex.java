8267946c798b51015402151e3fced19f2a9c62e1
Egor <yegeraskin13@gmail.com>
2020-04-06 15:50:07 +0400

    Add private field "edges" in class "Vertex"

--- src/main/java/ru/job4j/util/graph/Vertex.java
+++ src/main/java/ru/job4j/util/graph/Vertex.java
@@ -1,9 +1,13 @@
 package ru.job4j.util.graph;
 
+import java.util.List;
+
 //TODO JavaDoc
 public class Vertex {
     //TODO JavaDoc
     private int id;
     //TODO JavDoc
     private boolean usedStatus;
+    //TODO JavaDoc
+    private List<Vertex> edges;
 }
