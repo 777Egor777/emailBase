8381a8ba8d88226c81c5a18da69c4e75bfa891f1
Egor <yegeraskin13@gmail.com>
2020-04-06 17:05:30 +0400

    Add field "vertexes" in class "Graph"

--- src/main/java/ru/job4j/util/graph/Graph.java
+++ src/main/java/ru/job4j/util/graph/Graph.java
@@ -1,7 +1,11 @@
 package ru.job4j.util.graph;
 
+import java.util.List;
+
 //TODO JavaDoc
 public class Graph {
     //TODO JavaDoc
     private int numberOfVertex;
+    //TODO JavaDoc
+    private List<Vertex> vertexes;
 }
