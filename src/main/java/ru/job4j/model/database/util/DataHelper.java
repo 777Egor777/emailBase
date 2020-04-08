e8010571d90c7f8d3e4133ba146ff78feab25b3b
Egor <yegeraskin13@gmail.com>
2020-04-08 22:39:15 +0400

    Add frame of method "addUser" in class "DataHelper"

--- src/main/java/ru/job4j/model/database/util/DataHelper.java
+++ src/main/java/ru/job4j/model/database/util/DataHelper.java
@@ -1,6 +1,11 @@
 package ru.job4j.model.database.util;
 
+import ru.job4j.model.database.Data;
+import ru.job4j.model.user.User;
+
 //TODO JavaDoc
 public class DataHelper {
-    //TODO add methods
+    //TODO JavaDoc
+    public static void addUser(Data data, User user) {
+    }
 }
