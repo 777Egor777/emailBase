655bef59b8b17c356ae1b0391a5c5c3b5ad5227f
Egor <yegeraskin13@gmail.com>
2020-04-08 23:17:06 +0400

    Make class "ConsoleInput" implements "Input" interface Also add frame of method "askLine()"

--- src/main/java/ru/job4j/input/ConsoleInput.java
+++ src/main/java/ru/job4j/input/ConsoleInput.java
@@ -1,6 +1,22 @@
 package ru.job4j.input;
 
 //TODO Add JavaDoc
-public class ConsoleInput {
-    //TODO make "implmenets Input" and override methods
+public class ConsoleInput implements Input {
+    //TODO fix JavaDoc
+    /**
+     * Method receive String
+     * from some input essence
+     * and return it.
+     * <p>
+     * Method has no validation.
+     * It receive line in
+     * "fresh" condition
+     *
+     * @return line - object of String
+     * class
+     */
+    @Override
+    public String askLine() {
+        return null;
+    }
 }
