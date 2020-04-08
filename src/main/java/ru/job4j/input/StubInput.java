d0103784eec035d57a7af4ebd978e165bc58ef7d
Egor <yegeraskin13@gmail.com>
2020-04-08 23:31:09 +0400

    Make class "StubInput" implements "Input" interface Also add frame of method "askLine()"

--- src/main/java/ru/job4j/input/StubInput.java
+++ src/main/java/ru/job4j/input/StubInput.java
@@ -1,6 +1,22 @@
 package ru.job4j.input;
 
 //TODO Add JavaDoc
-public class StubInput {
-    //TODO make "implements Input" and override methods
+public class StubInput implements Input {
+    //TODO Fix JavaDoc
+    /**
+     * Method receive String
+     * from some input essence
+     * and return it.
+     * <p>
+     * Method has no validation.
+     * It receive line in
+     * "fresh" condition
+     *
+     * @return line - object of String
+     * class
+     */
+    @Override
+    public String askLine() {
+        return null;
+    }
 }
