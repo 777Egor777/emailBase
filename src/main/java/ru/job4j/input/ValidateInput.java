007c4b7d1c6ebb6f91506465e2dbf3bdd14f251a
Egor <yegeraskin13@gmail.com>
2020-04-08 23:32:27 +0400

    Make class "ValidateInput" implements "Input" interface Also add frame of method "askLine()"

--- src/main/java/ru/job4j/input/ValidateInput.java
+++ src/main/java/ru/job4j/input/ValidateInput.java
@@ -1,6 +1,22 @@
 package ru.job4j.input;
 
 //TODO JavaDoc
-public class ValidateInput {
-    //TODO make "implements Input"
+public class ValidateInput implements Input {
+    //TODO Fix JavaDoc
+    /**
+     * Method receive String
+     * from some input essence
+     * and return it.
+     * <p>
+     * Method has no validation.
+     * It receive line in
+     * "fresh" condition
+     *
+     * @return line - object of String
+     * class
+     */
+    @Override
+    public String askLine() {
+        return null;
+    }
 }
