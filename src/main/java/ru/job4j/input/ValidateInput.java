2e446726444a86eff41b4d73b8b0ed99b052e07c
Egor <yegeraskin13@gmail.com>
2020-04-09 11:13:59 +0400

    Add frame of method "askLine(String message, String... possibleLines)" in class "ValidateInput" Also add JavaDoc for it

--- src/main/java/ru/job4j/input/ValidateInput.java
+++ src/main/java/ru/job4j/input/ValidateInput.java
@@ -26,4 +26,46 @@ public class ValidateInput implements Input {
     public String askLine() {
         return null;
     }
+
+    //TODO Override methods
+
+    /**
+     * Method receive String
+     * from some input essence
+     * and return it,
+     * if line contains
+     * in array "possibleLines"
+     * <p>
+     * Method also can use
+     * "message" that consist
+     * some information about
+     * desired line
+     * <p>
+     * Method has validation.
+
+     * Method will ask lines
+     * until it receive valid
+     * line
+     *
+     * @param message       - String that consist
+     *                      some information about
+     *                      desired line. For example
+     *                      we can print it to console
+     *                      and user can read it
+     * @param possibleLines - array of lines, that
+     *                      we can return in this
+     *                      method. So we can return
+     *                      line only if it contains
+     *                      in this array.
+     *                      Method will receive new
+     *                      lines until it receive
+     *                      valid line
+     * @return line - object of String
+     * class that we receive
+     * from user
+     */
+    @Override
+    public String askLine(String message, String... possibleLines) {
+        return null;
+    }
 }
