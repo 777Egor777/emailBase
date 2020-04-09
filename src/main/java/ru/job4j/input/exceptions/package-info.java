74c63d75726ac9c29234eef843a5d604d44721c7
Egor <yegeraskin13@gmail.com>
2020-04-09 14:42:12 +0400

    Add JavaDoc for "package-info" file "input.exceptions"

--- src/main/java/ru/job4j/input/exceptions/package-info.java
+++ src/main/java/ru/job4j/input/exceptions/package-info.java
@@ -1,2 +1,9 @@
-//TODO JavaDoc
+/**
+ * In this package all
+ * exceptions files that
+ * thrown when received
+ * incorrect data in
+ * program
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ */
 package ru.job4j.input.exceptions;
