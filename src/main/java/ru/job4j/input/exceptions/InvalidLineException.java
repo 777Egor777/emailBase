4281109109aa92a44a048aecf9e6eef4c513db2b
Egor <yegeraskin13@gmail.com>
2020-04-09 14:36:29 +0400

    Add frame of constructor in class "InvalidLineExcept"

--- src/main/java/ru/job4j/input/exceptions/InvalidLineException.java
+++ src/main/java/ru/job4j/input/exceptions/InvalidLineException.java
@@ -1,10 +1,14 @@
 package ru.job4j.input.exceptions;
 
+import com.sun.jdi.InvalidLineNumberException;
+
 /**
  * This exception throw, when
  * received line is invalid
  * @author Geraskin Yegor(yegeraskin13@gmail.com)
  */
 public class InvalidLineException extends IllegalArgumentException {
-    //TODO Constructor
+    //TODO JavaDoc
+    public InvalidLineException(String message) {
+    }
 }
