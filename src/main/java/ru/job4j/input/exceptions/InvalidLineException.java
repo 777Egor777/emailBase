902812ff17993d1865e35f285f2662cf949cd7c0
Egor <yegeraskin13@gmail.com>
2020-04-09 14:37:05 +0400

    Add JavaDoc for constructor in class "InvalidLineExcept"

--- src/main/java/ru/job4j/input/exceptions/InvalidLineException.java
+++ src/main/java/ru/job4j/input/exceptions/InvalidLineException.java
@@ -8,7 +8,14 @@ import com.sun.jdi.InvalidLineNumberException;
  * @author Geraskin Yegor(yegeraskin13@gmail.com)
  */
 public class InvalidLineException extends IllegalArgumentException {
-    //TODO JavaDoc
+    /**
+     * Constructor just call same
+     * constructor of super-class
+     * @param message - {@code String} object that
+     *                  we send for super-class
+     *                  as message of this
+     *                  {@code Exception}
+     */
     public InvalidLineException(String message) {
     }
 }
