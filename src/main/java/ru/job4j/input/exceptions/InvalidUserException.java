30daadab7bd79bb82526849497bcf8f7e0361e1e
Egor <yegeraskin13@gmail.com>
2020-04-09 14:40:16 +0400

    Add JavaDoc constructor in class "InvalidUserExcept"

--- src/main/java/ru/job4j/input/exceptions/InvalidUserException.java
+++ src/main/java/ru/job4j/input/exceptions/InvalidUserException.java
@@ -7,7 +7,14 @@ package ru.job4j.input.exceptions;
  * @author Geraskin Yegor(yegeraskin13@gmail.com)
  */
 public class InvalidUserException extends IllegalArgumentException {
-    //TODO JavaDoc
+    /**
+     * Constructor just call same
+     * constructor of super-class
+     * @param message - {@code String} object that
+     *                  we send for super-class
+     *                  as message of this
+     *                  {@code Exception}
+     */
     public InvalidUserException(String message) {
     }
 }
