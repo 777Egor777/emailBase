99f72625d705dbad0fc735410feaa24ed70ed6dd
Egor <yegeraskin13@gmail.com>
2020-04-11 00:00:07 +0400

    Change JavaDoc of method "toString" in class "Data"

--- src/main/java/ru/job4j/model/database/Data.java
+++ src/main/java/ru/job4j/model/database/Data.java
@@ -66,25 +66,17 @@ public class Data {
     public void addUser(User user) {
     }
 
-    //TODO change JavaDoc
     /**
      * Returns a string representation of the object. In general, the
      * {@code toString} method returns a string that
      * "textually represents" this object. The result should
      * be a concise but informative representation that is easy for a
      * person to read.
-     * It is recommended that all subclasses override this method.
-     * <p>
-     * The {@code toString} method for class {@code Object}
-     * returns a string consisting of the name of the class of which the
-     * object is an instance, the at-sign character `{@code @}', and
-     * the unsigned hexadecimal representation of the hash code of the
-     * object. In other words, this method returns a string equal to the
-     * value of:
-     * <blockquote>
-     * <pre>
-     * getClass().getName() + '@' + Integer.toHexString(hashCode())
-     * </pre></blockquote>
+     * Finally, string representation of this object will have
+     * such format: User#id_1: Email1,Email2,...,EmailN_1
+     *              User#id_2: Email1,Email2,...,EmailN_2
+     *              ...
+     *              User#id_n: Email1,Email2,...,EmailN_n
      *
      * @return a string representation of the object.
      */
