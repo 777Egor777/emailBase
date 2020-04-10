d8285dbf23adcb31a0ccd136fc37b22986c57743
Egor <yegeraskin13@gmail.com>
2020-04-11 01:09:28 +0400

    Add JavaDoc for package-info "util.graph" package

--- src/main/java/ru/job4j/util/graph/package-info.java
+++ src/main/java/ru/job4j/util/graph/package-info.java
@@ -1,2 +1,6 @@
-//TODO JavaDoc
+/**
+ * In this package - all {@code class}'es
+ * connected with data model {@code Graph}
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ */
 package ru.job4j.util.graph;
