d99bb887fad8aec742f909fd5f2a75ba093d8cb7
Egor <yegeraskin13@gmail.com>
2020-04-10 23:47:15 +0400

    Add JavaDoc for package-info in "database.util"

--- src/main/java/ru/job4j/model/database/util/package-info.java
+++ src/main/java/ru/job4j/model/database/util/package-info.java
@@ -1,2 +1,15 @@
-//TODO JavaDoc
+/**
+ * Util package for {@code Data}
+ * {@code class}.
+ * Here we have {@code class}
+ * {@code DataHelper} that has
+ * methods-wrappers for
+ * method of {@code class}
+ * {@code Data}.
+ * We need it for future
+ * modofication of the
+ * project - class Data will
+ * become Immutable.
+ * @author Geradkin Yegor(yegeraskin13@gmail.com)
+ */
 package ru.job4j.model.database.util;
