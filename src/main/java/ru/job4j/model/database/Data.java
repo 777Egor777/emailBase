9a42076ac15b38f5fe905a6a530f9ec6ef3be748
Egor <yegeraskin13@gmail.com>
2020-04-11 00:16:40 +0400

    Add JavaDoc for method "compressBase" in class "Data"

--- src/main/java/ru/job4j/model/database/Data.java
+++ src/main/java/ru/job4j/model/database/Data.java
@@ -174,7 +174,94 @@ public class Data {
     public void deleteEmail(int id, int emailIndex) {
     }
 
-    //TODO JavaDoc
+    /**
+     * <p>
+     * Compress database.
+     * If user's have same
+     * email in base, we will
+     * join them. Unity object
+     * will have id of
+     * one of this {@code User}
+     * objects (any of two).
+     * We will do this process
+     * until all emails in base
+     * will belong to only one user.
+     * </p><p>
+     * This is simple algorithm,
+     * that explain what this
+     * method do.
+     * </p><p>
+     * In fact we will use
+     * effective graph algorithm
+     * that works in linear
+     * time.
+     * </p><p>
+     * We build graph, where
+     * each user is vertux,
+     * and edge between two users
+     * consist only if they
+     * have common email in base.
+     * </p><p>
+     * But if user's have same
+     * email in base they
+     * not mandatary have edge
+     * between each other.
+     * We place edge only between
+     * neighbour users by this email.
+     * </p><p>
+     * For examlpe:
+     * user1: email1, email2, email10
+     * user2: email3, email 4
+     * user3: email1, email5
+     * user4: email6
+     * user5: email1
+     * We will place edge between
+     * user1 and user3
+     * user3 and user5.
+     * Couse the are neighbors
+     * by common email "email1"
+     * </p><p>
+     * User1 and user5 has same
+     * "email1".
+     * We will not place edge
+     * between user1 and user5,
+     * cause exist user between them,
+     * that has "email1" too
+     * </p><p>
+     * So next i write algorithm
+     * of building graph:
+     * Crate Map<Email, id>.
+     * Start for-cycle throw database
+     * (for i: users
+     *   for j: i-emails)
+     * if next email already contains
+     * in base, make edge in graph
+     * between two users:
+     * this user and user map points
+     * to.
+     * </p><p>
+     * We use hashMap, so building
+     * of graph costs linear time
+     * for us.
+     * </p><p>
+     * Now graph has N vertuxes(
+     * N = number of users)
+     * and M edges(M = sum of all
+     * email-lists, of all users)
+     * in the worst case.
+     * </p><p>
+     * We need to extract components
+     * of connection in this graph.
+     * We will make it with help of
+     * algorithm DFS(Depth-First-
+     * Search). It works in linear
+     * time O(N+M).
+     * </p><p>
+     * Finally we get int array,
+     * which gives information about
+     * compression. Exactly in which
+     * user we must merge this user.
+     */
     public void compressBase() {
     }
 }
