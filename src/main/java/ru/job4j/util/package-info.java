fd5ddb44c71f8ec6a880af2fb00ee3a16e72a702
Egor <yegeraskin13@gmail.com>
2020-04-11 01:04:56 +0400

    Add JavaDoc for package-info "util" package

--- src/main/java/ru/job4j/util/package-info.java
+++ src/main/java/ru/job4j/util/package-info.java
@@ -1,2 +1,12 @@
-//TODO JavaDoc
+/**
+ * In this package all
+ * {@code class}'es that
+ * execute utility-function.
+ * For example, they can
+ * implement necessary
+ * algorithm(dfs) or model
+ * of such essence
+ * {@code Graph}
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ */
 package ru.job4j.util;
