d5760cea3f6f3a6fc211c8cffa170a272132605a
Egor <yegeraskin13@gmail.com>
2020-04-11 23:03:40 +0400

    Add test-class for class "User"

---
+++ src/test/java/ru/job4j/model/user/UserTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.model.user;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+public class UserTest {
+
+    @Test
+    public void getEmails() {
+    }
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void cleanEmailsList() {
+    }
+
+    @Test
+    public void getNumberOfEmails() {
+    }
+
+    @Test
+    public void getEmail() {
+    }
+
+    @Test
+    public void deleteEmail() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+
+    @Test
+    public void addEmail() {
+    }
+}
