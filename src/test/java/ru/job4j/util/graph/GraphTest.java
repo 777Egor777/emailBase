cc259088ad6cbef1ab68a04b99a7539238fa1746
Egor <yegeraskin13@gmail.com>
2020-04-11 21:42:25 +0400

    Add Test-class for class "Graph"

---
+++ src/test/java/ru/job4j/util/graph/GraphTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.util.graph;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Test class for class "Graph"
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ */
+public class GraphTest {
+
+    @Test
+    public void addEdge() {
+    }
+
+    @Test
+    public void getSources() {
+    }
+}
