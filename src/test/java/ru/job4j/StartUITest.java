228fd9bdc9e13b625d2bc01afceccff5dc2c2a30
Egor <yegeraskin13@gmail.com>
2020-04-11 15:57:29 +0400

    Create StartUITest class

---
+++ src/test/java/ru/job4j/StartUITest.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Test class for class "StartUI"
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ */
+public class StartUITest {
+
+}
