1bc245a292b613e306e9c724c8b5ee64fe2ef1ee
Egor <yegeraskin13@gmail.com>
2020-04-11 23:12:43 +0400

    Tests for "getEmails()" class "User"

--- src/test/java/ru/job4j/model/user/UserTest.java
+++ src/test/java/ru/job4j/model/user/UserTest.java
@@ -1,13 +1,41 @@
 package ru.job4j.model.user;
 
 import org.junit.Test;
+import ru.job4j.model.email.Email;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.is;
 
 public class UserTest {
 
     @Test
-    public void getEmails() {
+    public void getEmails1() {
+        User user = new User();
+        List<Email> emails = new ArrayList<>();
+        assertThat(user.getEmails(),
+                   is(emails));
+    }
+
+    @Test
+    public void getEmails2() {
+        User user = new User();
+        Email[] q = new Email[]{
+                new Email("email1@yandex.ru"),
+                new Email("email2@gmail.com"),
+                new Email("email10@rambler.net")
+        };
+
+        for(Email email : q) {
+            user.addEmail(email);
+        }
+
+        List<Email> emails = new ArrayList<>(Arrays.asList(q));
+        assertThat(user.getEmails(),
+                is(emails));
     }
 
     @Test
