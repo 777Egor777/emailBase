d9f46e9094078778bdbf93dbfbb404766b9aa199
Egor <yegeraskin13@gmail.com>
2020-04-12 01:03:11 +0400

    Create test-class for class "Users"

---
+++ src/test/java/ru/job4j/model/user/util/UsersTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.model.user.util;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Test class for class "Users"
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ */
+public class UsersTest {
+    @Test
+    public void cleanEmailsList() {
+    }
+
+    @Test
+    public void printEmails() {
+    }
+
+    @Test
+    public void deleteEmail() {
+    }
+
+    @Test
+    public void addEmail() {
+    }
+}
