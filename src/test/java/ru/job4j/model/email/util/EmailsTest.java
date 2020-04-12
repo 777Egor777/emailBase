28b91fabd7172d8621041341706ae2833172845d
Egor <yegeraskin13@gmail.com>
2020-04-12 20:49:07 +0400

    Add test for method isCorrectDomain class Emails

--- src/test/java/ru/job4j/model/email/util/EmailsTest.java
+++ src/test/java/ru/job4j/model/email/util/EmailsTest.java
@@ -27,6 +27,162 @@ public class EmailsTest {
     }
 
     @Test
-    public void isCorrectDomain() {
+    public void isCorrectDomain1() {
+        String domain = "";
+        boolean result = Emails.isCorrectDomain(domain);
+        boolean expected = false;
+        assertThat(result, is(expected));
     }
+
+    @Test
+    public void isCorrectDomain2() {
+        String domain = "ru";
+        boolean result = Emails.isCorrectDomain(domain);
+        boolean expected = true;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void isCorrectDomain3() {
+        String domain = "com";
+        boolean result = Emails.isCorrectDomain(domain);
+        boolean expected = true;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void isCorrectDomain4() {
+        String domain = "net";
+        boolean result = Emails.isCorrectDomain(domain);
+        boolean expected = true;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void isCorrectDomain5() {
+        String domain = "org";
+        boolean result = Emails.isCorrectDomain(domain);
+        boolean expected = true;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void isCorrectDomain6() {
+        String domain = "edu";
+        boolean result = Emails.isCorrectDomain(domain);
+        boolean expected = true;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void isCorrectDomain7() {
+        String domain = ".ru";
+        boolean result = Emails.isCorrectDomain(domain);
+        boolean expected = false;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void isCorrectDomain8() {
+        String domain = ".com";
+        boolean result = Emails.isCorrectDomain(domain);
+        boolean expected = false;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void isCorrectDomain9() {
+        String domain = ".net";
+        boolean result = Emails.isCorrectDomain(domain);
+        boolean expected = false;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void isCorrectDomain10() {
+        String domain = ".edu";
+        boolean result = Emails.isCorrectDomain(domain);
+        boolean expected = false;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void isCorrectDomain11() {
+        String domain = ".org";
+        boolean result = Emails.isCorrectDomain(domain);
+        boolean expected = false;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void isCorrectDomain12() {
+        String domain = ".";
+        boolean result = Emails.isCorrectDomain(domain);
+        boolean expected = false;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void isCorrectDomain13() {
+        String domain = "@";
+        boolean result = Emails.isCorrectDomain(domain);
+        boolean expected = false;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void isCorrectDomain14() {
+        String domain = "yandex.ru";
+        boolean result = Emails.isCorrectDomain(domain);
+        boolean expected = false;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void isCorrectDomain15() {
+        String domain = "gmail.com";
+        boolean result = Emails.isCorrectDomain(domain);
+        boolean expected = false;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void isCorrectDomain16() {
+        String domain = "ру";
+        boolean result = Emails.isCorrectDomain(domain);
+        boolean expected = false;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void isCorrectDomain17() {
+        String domain = "qwe";
+        boolean result = Emails.isCorrectDomain(domain);
+        boolean expected = false;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void isCorrectDomain18() {
+        String domain = "qw";
+        boolean result = Emails.isCorrectDomain(domain);
+        boolean expected = false;
+        assertThat(result, is(expected));
+    }
+    @Test
+    public void isCorrectDomain19() {
+        String domain = "@ru";
+        boolean result = Emails.isCorrectDomain(domain);
+        boolean expected = false;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void isCorrectDomain20() {
+        String domain = ",ru";
+        boolean result = Emails.isCorrectDomain(domain);
+        boolean expected = false;
+        assertThat(result, is(expected));
+    }
+
 }
