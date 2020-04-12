3f5230ce6d18af88562226a20dbaabd4bd1d1e82
Egor <yegeraskin13@gmail.com>
2020-04-13 00:15:49 +0400

    Add tests for method "addUser" class "Data"

--- src/test/java/ru/job4j/model/database/DataTest.java
+++ src/test/java/ru/job4j/model/database/DataTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.model.database;
 
 import org.junit.Test;
+import ru.job4j.model.user.User;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.is;
 
@@ -11,7 +13,103 @@ import static org.hamcrest.Matchers.is;
 public class DataTest {
 
     @Test
-    public void addUser() {
+    public void addUser1() {
+        User user = new User();
+        Data data = new Data();
+        data.addUser(user);
+        assertThat(data.getUser(user.getId()), is(user));
+    }
+
+    @Test
+    public void addUser2() {
+        User user = new User();
+        Data data = new Data();
+        assertThat(data.getUser(user.getId()), is(Data.EMPTY_USER));
+    }
+
+    @Test
+    public void addUser3() {
+        User user = new User();
+        user = new User();
+        user = new User();
+        Data data = new Data();
+        assertThat(data.getUser(user.getId()), is(user));
+    }
+
+    @Test
+    public void addUser4() {
+        User user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        Data data = new Data();
+        assertThat(data.getUser(user.getId()), is(user));
+    }
+
+    @Test
+    public void addUser5() {
+        User user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        Data data = new Data();
+        assertThat(data.getUser(user.getId()), is(user));
+    }
+
+    @Test
+    public void addUser6() {
+        User user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        user = new User();
+        Data data = new Data();
+        assertThat(data.getUser(user.getId()), is(user));
+    }
+
+    @Test
+    public void addUser7() {
+        User user = new User();
+        for(int i=0; i < 100500; ++i) {
+            user = new User();
+        }
+        Data data = new Data();
+        assertThat(data.getUser(user.getId()), is(user));
+    }
+
+    @Test
+    public void addUser8() {
+        User[] users = new User[100500];
+        for(int i=0; i < 100500; ++i) {
+            users[i] = new User();
+        }
+        Data data = new Data();
+        assertThat(data.getUser(users[77777].getId()), is(users[77777]));
     }
 
     @Test
