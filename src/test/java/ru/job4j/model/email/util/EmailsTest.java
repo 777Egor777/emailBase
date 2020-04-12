34feb4e58f185271247347a99e362ebcd46e9f76
Egor <yegeraskin13@gmail.com>
2020-04-12 20:40:58 +0400

    Add test-class for class "Emails"

---
+++ src/test/java/ru/job4j/model/email/util/EmailsTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.model.email.util;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Test class for class "Emails"
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ */
+public class EmailsTest {
+
+    @Test
+    public void isCorrectEmail() {
+    }
+
+    @Test
+    public void isCorrectValue() {
+    }
+
+    @Test
+    public void isCorrectName() {
+    }
+
+    @Test
+    public void isCorrectService() {
+    }
+
+    @Test
+    public void isCorrectDomain() {
+    }
+}
