7af131dedf4d0ecfbb2de96aaa1284fd0c8179ec
Egor <yegeraskin13@gmail.com>
2020-04-13 00:08:11 +0400

    Create test-class for class "Data"

---
+++ src/test/java/ru/job4j/model/database/DataTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.model.database;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Test class for class "Data"
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ */
+public class DataTest {
+
+    @Test
+    public void addUser() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+
+    @Test
+    public void clean() {
+    }
+
+    @Test
+    public void addEmail() {
+    }
+
+    @Test
+    public void allId() {
+    }
+
+    @Test
+    public void deleteUser() {
+    }
+
+    @Test
+    public void getUser() {
+    }
+
+    @Test
+    public void deleteEmail() {
+    }
+
+    @Test
+    public void compressBase() {
+    }
+}
