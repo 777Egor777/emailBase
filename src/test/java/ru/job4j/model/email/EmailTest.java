22a76c013298da9496d556bcf13c8e30d1fb563a
Egor <yegeraskin13@gmail.com>
2020-04-12 15:26:57 +0400

    Tests for method "hashCode" class "Email"

--- src/test/java/ru/job4j/model/email/EmailTest.java
+++ src/test/java/ru/job4j/model/email/EmailTest.java
@@ -101,7 +101,93 @@ public class EmailTest {
     }
 
     @Test
-    public void testHashCode() {
+    public void testHashCode1() {
+        String value = "yegeraskin13@gmail.com";
+        Email email = new Email(value);
+        int result = email.hashCode();
+        int expected = value.hashCode();
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void testHashCode2() {
+        String value = "";
+        Email email = new Email(value);
+        int result = email.hashCode();
+        int expected = value.hashCode();
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void testHashCode3() {
+        String value = "yegeraskin13@rambler.com";
+        Email email = new Email(value);
+        int result = email.hashCode();
+        int expected = value.hashCode();
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void testHashCode4() {
+        String value = "xxx@yyy.org";
+        Email email = new Email(value);
+        int result = email.hashCode();
+        int expected = value.hashCode();
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void testHashCode5() {
+        String value = "xxx@yyy.edu";
+        Email email = new Email(value);
+        int result = email.hashCode();
+        int expected = value.hashCode();
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void testHashCode6() {
+        String value = "qw@po.net";
+        Email email = new Email(value);
+        int result = email.hashCode();
+        int expected = value.hashCode();
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void testHashCode7() {
+        String value = "eg@mipt.ru";
+        Email email = new Email(value);
+        int result = email.hashCode();
+        int expected = value.hashCode();
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void testHashCode8() {
+        String value = "@serv.";
+        Email email = new Email(value);
+        int result = email.hashCode();
+        int expected = value.hashCode();
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void testHashCode9() {
+        String value = "@";
+        Email email = new Email(value);
+        int result = email.hashCode();
+        int expected = value.hashCode();
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void testHashCode10() {
+        String value = "fglsfgj ;lskj erioj reigj aeropi _";
+        Email email = new Email(value);
+        int result = email.hashCode();
+        int expected = value.hashCode();
+        assertThat(result, is(expected));
     }
 
     @Test
