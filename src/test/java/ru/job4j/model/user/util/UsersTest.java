69dc18b9742780d625f2c39249df68e5db5ab0fd
Egor <yegeraskin13@gmail.com>
2020-04-12 12:04:55 +0400

    Tests for method "cleanEmailsList" class "Users"

--- src/test/java/ru/job4j/model/user/util/UsersTest.java
+++ src/test/java/ru/job4j/model/user/util/UsersTest.java
@@ -1,6 +1,9 @@
 package ru.job4j.model.user.util;
 
 import org.junit.Test;
+import ru.job4j.model.email.Email;
+import ru.job4j.model.user.User;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.is;
 
@@ -10,7 +13,70 @@ import static org.hamcrest.Matchers.is;
  */
 public class UsersTest {
     @Test
-    public void cleanEmailsList() {
+    public void cleanEmailsList1() {
+        User user = new User();
+        Users.cleanEmailsList(user);
+        assertThat(user.getNumberOfEmails(), is(0));
+    }
+
+    @Test
+    public void cleanEmailsList2() {
+        User user = new User();
+        Users.addEmail(user, new Email("yegerasskin13@gmail.com"));
+        Users.cleanEmailsList(user);
+        assertThat(user.getNumberOfEmails(), is(0));
+    }
+
+    @Test
+    public void cleanEmailsList3() {
+        User user = new User();
+        Users.addEmail(user, new Email("yegerasskin13@gmail.com"));
+        Users.addEmail(user, new Email("tiptop123@rambler.ru"));
+        Users.cleanEmailsList(user);
+        assertThat(user.getNumberOfEmails(), is(0));
+    }
+
+    @Test
+    public void cleanEmailsList4() {
+        User user = new User();
+        Users.addEmail(user, new Email("yegerasskin13@gmail.com"));
+        Users.addEmail(user, new Email("tiptop123@rambler.ru"));
+        Users.addEmail(user, new Email("ivanraptor@yandex.ru"));
+        Users.cleanEmailsList(user);
+        assertThat(user.getNumberOfEmails(), is(0));
+    }
+
+    @Test
+    public void cleanEmailsList5() {
+        User user = new User();
+        Users.addEmail(user, new Email("yegerasskin13@gmail.com"));
+        Users.addEmail(user, new Email("tiptop123@rambler.ru"));
+        Users.addEmail(user, new Email("ivanraptor@yandex.ru"));
+        Users.addEmail(user, new Email("ivanraptor@yandex.ru"));
+        Users.addEmail(user, new Email("asdasdas@yandex.ru"));
+        Users.addEmail(user, new Email("xcvbdfgsd@yandex.ru"));
+        Users.addEmail(user, new Email("srrthjdfh@werqe.edu"));
+        Users.addEmail(user, new Email("ghjjfghfs@rambler.org"));
+        Users.cleanEmailsList(user);
+        assertThat(user.getNumberOfEmails(), is(0));
+    }
+
+    @Test
+    public void cleanEmailsList6() {
+        User user = new User();
+        Users.addEmail(user, new Email("yegerasskin13@gmail.com"));
+        Users.addEmail(user, new Email("tiptop123@rambler.ru"));
+        Users.addEmail(user, new Email("ivanraptor@yandex.ru"));
+        Users.addEmail(user, new Email("ivanraptor@yandex.ru"));
+        Users.addEmail(user, new Email("asdasdas@yandex.ru"));
+        Users.addEmail(user, new Email("xcvbdfgsd@yandex.ru"));
+        Users.addEmail(user, new Email("srrthjdfh@werqe.edu"));
+        Users.addEmail(user, new Email("ghjjfghfs@rambler.org"));
+        for(int index = 0; index < 100500; ++index) {
+            Users.addEmail(user, new Email("user" + index + "@yandex.ru"));
+        }
+        Users.cleanEmailsList(user);
+        assertThat(user.getNumberOfEmails(), is(0));
     }
 
     @Test
