1f0040e18f6bb42e4a7d45ff89ca8c24d3021442
Egor <yegeraskin13@gmail.com>
2020-04-12 15:10:40 +0400

    Test-class for class "Email"

---
+++ src/test/java/ru/job4j/model/email/EmailTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.model.email;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Test class for class "Email"
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ */
+public class EmailTest {
+
+    @Test
+    public void testToString() {
+    }
+
+    @Test
+    public void testHashCode() {
+    }
+
+    @Test
+    public void testEquals() {
+    }
+
+    @Test
+    public void getValue() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getService() {
+    }
+
+    @Test
+    public void getDomain() {
+    }
+}
