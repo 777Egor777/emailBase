42e78aa91e36b7c4baeb7da0e2bc37a806a8da48
Egor <yegeraskin13@gmail.com>
2020-04-13 13:05:21 +0400

    Add test-class for "Input" interface

---
+++ src/test/java/ru/job4j/input/InputTest.java
@@ -0,0 +1,71 @@
+package ru.job4j.input;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Test class for interface
+ * {@code Input}, that has
+ * default realisations
+ * of methods
+ * @auhtor Geraskin Yegor(yegeraskin13@gmail.com)
+ */
+public class InputTest {
+
+    @Test
+    public void askLine() {
+    }
+
+    @Test
+    public void testAskLine() {
+    }
+
+    @Test
+    public void testAskLine1() {
+    }
+
+    @Test
+    public void askInt() {
+    }
+
+    @Test
+    public void testAskInt() {
+    }
+
+    @Test
+    public void testAskInt1() {
+    }
+
+    @Test
+    public void testAskInt2() {
+    }
+
+    @Test
+    public void testAskInt3() {
+    }
+
+    @Test
+    public void askEmail() {
+    }
+
+    @Test
+    public void testAskEmail() {
+    }
+
+    @Test
+    public void testAskEmail1() {
+    }
+
+    @Test
+    public void askUser() {
+    }
+
+    @Test
+    public void testAskUser() {
+    }
+
+    @Test
+    public void askData() {
+    }
+}
