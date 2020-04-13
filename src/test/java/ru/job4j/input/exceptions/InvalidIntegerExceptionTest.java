d065badd379e52faa15a1c7b9c4c13bd3dfad250
Egor <yegeraskin13@gmail.com>
2020-04-13 12:49:26 +0400

    Add test-class for "InvalidIntegerException"

---
+++ src/test/java/ru/job4j/input/exceptions/InvalidIntegerExceptionTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.input.exceptions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test class for class "InvalidIntegerException"
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ */
+public class InvalidIntegerExceptionTest {
+    @Test(expected = InvalidIntegerException.class)
+    public void InvalidIntegerExceptionTest() {
+        throw new InvalidIntegerException("message");
+    }
+}
