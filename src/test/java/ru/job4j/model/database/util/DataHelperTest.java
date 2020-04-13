4db166aeeffd63b50e2609d53bf29d3d9cf62bad
Egor <yegeraskin13@gmail.com>
2020-04-13 12:05:08 +0400

    Add test-class for class "DataHelper"

---
+++ src/test/java/ru/job4j/model/database/util/DataHelperTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.model.database.util;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Test class for class "DataHelper"
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ */
+public class DataHelperTest {
+
+    @Test
+    public void addUser() {
+    }
+
+    @Test
+    public void clean() {
+    }
+
+    @Test
+    public void addEmail() {
+    }
+
+    @Test
+    public void deleteUser() {
+    }
+
+    @Test
+    public void deleteEmail() {
+    }
+
+    @Test
+    public void compressBase() {
+    }
+}
