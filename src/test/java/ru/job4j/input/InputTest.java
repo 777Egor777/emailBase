9d225f1f243c4d60f9fd6577ec407a8aa752602c
Egor <yegeraskin13@gmail.com>
2020-04-13 21:31:01 +0400

    Tests. Class "InputTest". Method "askLine"

--- src/test/java/ru/job4j/input/InputTest.java
+++ src/test/java/ru/job4j/input/InputTest.java
@@ -1,6 +1,9 @@
 package ru.job4j.input;
 
 import org.junit.Test;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.is;
 
@@ -14,7 +17,90 @@ import static org.hamcrest.Matchers.is;
 public class InputTest {
 
     @Test
-    public void askLine() {
+    public void askLine1() {
+        String line = "";
+        Input input = new Input() {
+            private Input input = new StubInput(Arrays.asList(
+                new String(line)
+            ));
+            @Override
+            public String askLine() {
+                return input.askLine();
+            }
+        };
+        assertThat(input.askLine(), is(line));
+    }
+
+    @Test
+    public void askLine2() {
+        String line = "ssdfasdfsaf";
+        Input input = new Input() {
+            private Input input = new StubInput(Arrays.asList(
+                    new String(line)
+            ));
+            @Override
+            public String askLine() {
+                return input.askLine();
+            }
+        };
+        assertThat(input.askLine(), is(line));
+    }
+
+    @Test
+    public void askLine3() {
+        String line = "";
+        Input input = new Input() {
+            private Input input = new StubInput(Arrays.asList(
+                    new String(line),
+                    "qwerty"
+            ));
+            @Override
+            public String askLine() {
+                return input.askLine();
+            }
+        };
+        input.askLine();
+        assertThat(input.askLine(), is("qwerty"));
+    }
+
+    @Test
+    public void askLine4() {
+        String line = "";
+        Input input = new Input() {
+            private Input input = new StubInput(Arrays.asList(
+                    new String(line),
+                    "qwerty",
+                    "zxczxcsgfghhfgh"
+            ));
+            @Override
+            public String askLine() {
+                return input.askLine();
+            }
+        };
+        input.askLine();
+        input.askLine();
+        assertThat(input.askLine(), is("zxczxcsgfghhfgh"));
+    }
+
+    @Test
+    public void askLine5() {
+        String[] lines = new String[100500];
+        for(int i = 0; i < 100500; ++i) {
+            lines[i] = "line#" + i;
+        }
+        Input input = new Input() {
+            private Input input = new StubInput(Arrays.asList(
+                    lines
+            ));
+            @Override
+            public String askLine() {
+                return input.askLine();
+            }
+        };
+        for(int i = 0; i < 33333; ++i) {
+            input.askLine();
+        }
+        assertThat(input.askLine(), is(lines[33334]));
     }
 
     @Test
