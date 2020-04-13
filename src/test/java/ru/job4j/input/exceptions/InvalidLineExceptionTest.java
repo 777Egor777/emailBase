cb100800541da297bb2221481cff4ccbd1e7ed91
Egor <yegeraskin13@gmail.com>
2020-04-13 12:54:27 +0400

    Add test-class for "InvalidLineException"

---
+++ src/test/java/ru/job4j/input/exceptions/InvalidLineExceptionTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.input.exceptions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test class for class "InvalidLineException"
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ */
+public class InvalidLineExceptionTest {
+    @Test(expected = InvalidLineException.class)
+    public void InvalidLineExceptionTest() {
+        throw new InvalidLineException("message");
+    }
+}
