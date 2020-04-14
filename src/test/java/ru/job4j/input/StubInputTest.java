9610077be5d636a1875dcdc80aa62778aa7d2e34
Egor <yegeraskin13@gmail.com>
2020-04-15 00:11:37 +0400

    Tests "StubInputTest" method "askLine()"

--- src/test/java/ru/job4j/input/StubInputTest.java
+++ src/test/java/ru/job4j/input/StubInputTest.java
@@ -13,4 +13,38 @@ import static org.hamcrest.Matchers.is;
  * @author Geraskin Yegor(yegeraskin13@gmail.com)
  */
 public class StubInputTest {
+    @Test
+    public void askLine1() {
+        Input input = new StubInput(Arrays.asList(
+                "Egor"
+        ));
+        assertThat(input.askLine(), is("Egor"));
+    }
+
+    @Test
+    public void askLine2() {
+        Input input = new StubInput(Arrays.asList(
+                "Egor",
+                "Ivan"
+        ));
+        input.askLine();
+        assertThat(input.askLine(), is("Ivan"));
+    }
+
+    @Test
+    public void askLine3() {
+        Input input = new StubInput(Arrays.asList(
+                "Egor",
+                "Ivan",
+                "Petr",
+                "Petr",
+                "Alex",
+                "Serg"
+        ));
+        input.askLine();
+        input.askLine();
+        input.askLine();
+        input.askLine();
+        assertThat(input.askLine(), is("Alex"));
+    }
 }
