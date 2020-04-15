0e7ebb96e259f44194a48f85134cb9c325644f3b
Egor <yegeraskin13@gmail.com>
2020-04-15 10:21:03 +0400

    Tests "ValidateInputTest", "askInt"

--- src/test/java/ru/job4j/input/ValidateInputTest.java
+++ src/test/java/ru/job4j/input/ValidateInputTest.java
@@ -2,7 +2,9 @@ package ru.job4j.input;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.is;
@@ -31,11 +33,164 @@ public class ValidateInputTest {
     }
 
     @Test
-    public void askInt() {
+    public void askInt1() {
+        Input input = new StubInput(Arrays.asList(
+                "-1000000"
+        ));
+        assertThat(input.askInt(),is(-1000000));
     }
 
     @Test
-    public void testAskInt() {
+    public void askInt2() {
+        Input input = new StubInput(Arrays.asList(
+                "-55"
+        ));
+        assertThat(input.askInt(),is(-55));
+    }
+
+    @Test
+    public void askInt3() {
+        Input input = new StubInput(Arrays.asList(
+                "0"
+        ));
+        assertThat(input.askInt(),is(0));
+    }
+
+    @Test
+    public void askInt4() {
+        Input input = new StubInput(Arrays.asList(
+                "1"
+        ));
+        assertThat(input.askInt(),is(1));
+    }
+
+    @Test
+    public void askInt5() {
+        Input input = new StubInput(Arrays.asList(
+                "2"
+        ));
+        assertThat(input.askInt(),is(2));
+    }
+
+    @Test
+    public void askInt6() {
+        Input input = new StubInput(Arrays.asList(
+                "3"
+        ));
+        assertThat(input.askInt(),is(3));
+    }
+
+    @Test
+    public void askInt7() {
+        Input input = new StubInput(Arrays.asList(
+                "7"
+        ));
+        assertThat(input.askInt(),is(7));
+    }
+
+    @Test
+    public void askInt8() {
+        Input input = new StubInput(Arrays.asList(
+                "100"
+        ));
+        assertThat(input.askInt(),is(100));
+    }
+
+    @Test
+    public void askInt9() {
+        Input input = new StubInput(Arrays.asList(
+                "1000000"
+        ));
+        assertThat(input.askInt(),is(1000000));
+    }
+
+    @Test
+    public void askInt10() {
+        Input input = new StubInput(Arrays.asList(
+                " 1000000 "
+        ));
+        assertThat(input.askInt(),is(1000000));
+    }
+
+    @Test
+    public void askInt11() {
+        Input input = new StubInput(Arrays.asList(
+                " 0 "
+        ));
+        assertThat(input.askInt(),is(0));
+    }
+
+    @Test()
+    public void askInt12() {
+        Input input = new StubInput(Arrays.asList(
+                "",
+                "-1000000"
+        ));
+        assertThat(input.askInt(),is(-1000000));
+    }
+
+    @Test
+    public void askInt13() {
+        Input input = new StubInput(Arrays.asList(
+                "555,555",
+                "-1000000"
+        ));
+        assertThat(input.askInt(),is(-1000000));
+    }
+
+    @Test
+    public void askInt14() {
+        Input input = new StubInput(Arrays.asList(
+                "null",
+                "-1000000"
+        ));
+        assertThat(input.askInt(),is(-1000000));
+    }
+
+    @Test
+    public void askInt15() {
+        Input input = new StubInput(Arrays.asList(
+                "10 000",
+                "-1000000"
+        ));
+        assertThat(input.askInt(),is(-1000000));
+    }
+
+    @Test
+    public void askInt16() {
+        Input input = new StubInput(Arrays.asList(
+                "yegeraskin13@gmail.com",
+                "-1000000"
+        ));
+        assertThat(input.askInt(),is(-1000000));
+    }
+
+    @Test
+    public void askInt17() {
+        Input input = new StubInput(Arrays.asList(
+                "1e",
+                "yegeraskin13@gmail.com",
+                "-1000000"
+        ));
+        assertThat(input.askInt(),is(-1000000));
+    }
+
+    @Test
+    public void askInt18() {
+        List<String> list = new ArrayList<>();
+        for(int i = 0; i < 100500; ++i) {
+            list.add("xxx" + i);
+        }
+        list.add("49");
+        list.add("xxx");
+        list.add("were");
+        list.add("dhwet");
+        Input input = new StubInput(list);
+        assertThat(input.askInt(),is(49));
+    }
+
+    @Test
+    public void testAskIntMessage() {
     }
 
     @Test
