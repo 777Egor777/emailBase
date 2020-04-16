95b5c43ccd414e3fbe6212c1b171e332d4578ffd
Egor <yegeraskin13@gmail.com>
2020-04-16 17:33:10 +0400

    Test class for class "PrintDataAction"

---
+++ src/test/java/ru/job4j/action/PrintDataActionTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.action;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Test class for class
+ * {@code PrintDataAction}
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ */
+
+public class PrintDataActionTest {
+
+    @Test
+    public void getTitle() {
+    }
+
+    @Test
+    public void execute() {
+    }
+}
