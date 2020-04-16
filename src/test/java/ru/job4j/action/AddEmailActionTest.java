bd825cfe2244a0a068f2629225da8c8ba8f069bb
Egor <yegeraskin13@gmail.com>
2020-04-17 00:42:30 +0400

    Add test class for class "AddEmailAction"

---
+++ src/test/java/ru/job4j/action/AddEmailActionTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.action;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Test class for class
+ * {@code AddEmailAction}
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ */
+public class AddEmailActionTest {
+
+    @Test
+    public void getTitle() {
+    }
+
+    @Test
+    public void execute() {
+    }
+}
