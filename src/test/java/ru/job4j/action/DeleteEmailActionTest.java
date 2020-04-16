f4593a262642bbc9684d0712bdf17a500602e40c
Egor <yegeraskin13@gmail.com>
2020-04-16 23:47:05 +0400

    Test for method "execute" class "DeleteEmailAction"

--- src/test/java/ru/job4j/action/DeleteEmailActionTest.java
+++ src/test/java/ru/job4j/action/DeleteEmailActionTest.java
@@ -1,6 +1,18 @@
 package ru.job4j.action;
 
 import org.junit.Test;
+import ru.job4j.input.Input;
+import ru.job4j.input.StubInput;
+import ru.job4j.input.ValidateInput;
+import ru.job4j.model.database.Data;
+import ru.job4j.model.database.util.DataHelper;
+import ru.job4j.model.email.Email;
+import ru.job4j.model.user.User;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.StringJoiner;
 
 import static org.junit.Assert.*;
 
@@ -16,10 +28,47 @@ public class DeleteEmailActionTest {
     @Test
     public void getTitle() {
         UserAction action = new DeleteEmailAction();
-        assertThat(action.getTitle(), is("DELETE USER"));
+        assertThat(action.getTitle(), is("DELETE EMAIL"));
     }
 
     @Test
-    public void execute() {
+    public void execute1() {
+        UserAction action = new DeleteEmailAction();
+        Data data = new Data();
+        User user = new User();
+        Email email = new Email("terekhovvados31@yandex.ru");
+        DataHelper.addUser(data, user);
+        DataHelper.addEmail(data, user.getId(), email);
+        Input input = new ValidateInput(new StubInput(Arrays.asList(
+                ""+user.getId(),
+                "1"
+        )));
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        StringJoiner joiner = new StringJoiner(System.lineSeparator(), "", System.lineSeparator());
+        PrintStream stdout = System.out;
+        System.setOut(new PrintStream(baos));
+
+        action.execute(input, data);
+        joiner.add("===DELETE EMAIL===");
+        joiner.add("Enter user's id: ");
+        joiner.add("Enter email's index: ");
+
+        System.setOut(stdout);
+        assertThat(baos.toString(), is(joiner.toString()));
+    }
+
+    @Test
+    public void execute2() {
+        UserAction action = new DeleteEmailAction();
+        Data data = new Data();
+        User user = new User();
+        Email email = new Email("terekhovvados31@yandex.ru");
+        DataHelper.addUser(data, user);
+        DataHelper.addEmail(data, user.getId(), email);
+        Input input = new ValidateInput(new StubInput(Arrays.asList(
+                ""+user.getId(),
+                "1"
+        )));
+        assertThat(action.execute(input, data), is(true));
     }
 }
