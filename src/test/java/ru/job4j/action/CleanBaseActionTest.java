bcb2d46313d884e8a74e3c5f8042422ce6b5b2a7
Egor <yegeraskin13@gmail.com>
2020-04-17 00:13:29 +0400

    Add test class for class "CleanBaseAction"

---
+++ src/test/java/ru/job4j/action/CleanBaseActionTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.action;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Test class for class
+ * {@code CleanBaseAction}
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ */
+public class CleanBaseActionTest {
+
+    @Test
+    public void getTitle() {
+    }
+
+    @Test
+    public void execute() {
+    }
+}
