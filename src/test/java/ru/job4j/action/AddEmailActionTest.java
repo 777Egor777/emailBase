001f5aba42ee9c7f2da287095097a132030013d1
Egor <yegeraskin13@gmail.com>
2020-04-17 00:51:03 +0400

    Add test for method "execute" class "AddEmailAction"

--- src/test/java/ru/job4j/action/AddEmailActionTest.java
+++ src/test/java/ru/job4j/action/AddEmailActionTest.java
@@ -1,6 +1,17 @@
 package ru.job4j.action;
 
 import org.junit.Test;
+import ru.job4j.input.Input;
+import ru.job4j.input.StubInput;
+import ru.job4j.input.ValidateInput;
+import ru.job4j.model.database.Data;
+import ru.job4j.model.database.util.DataHelper;
+import ru.job4j.model.user.User;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.StringJoiner;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.is;
@@ -19,6 +30,41 @@ public class AddEmailActionTest {
     }
 
     @Test
-    public void execute() {
+    public void execute1() {
+        UserAction action = new AddEmailAction();
+        Data data = new Data();
+        User user= new User();
+        DataHelper.addUser(data, user);
+        Input input = new ValidateInput(new StubInput(Arrays.asList(
+                ""+user.getId(),
+                "geraskin@phystech.edu"
+        )));
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        StringJoiner joiner = new StringJoiner(System.lineSeparator(), "", System.lineSeparator());
+        PrintStream stdout = System.out;
+        System.setOut(new PrintStream(baos));
+
+        action.execute(input, data);
+
+        joiner.add("===ADD EMAIL===");
+        joiner.add("Enter user's id: ");
+        joiner.add("Enter email: ");
+
+        System.setOut(stdout);
+        assertThat(baos.toString(), is(joiner.toString()));
+    }
+
+    @Test
+    public void execute2() {
+        UserAction action = new AddEmailAction();
+        Data data = new Data();
+        User user= new User();
+        DataHelper.addUser(data, user);
+        Input input = new ValidateInput(new StubInput(Arrays.asList(
+                ""+user.getId(),
+                "geraskin@phystech.edu"
+        )));
+        action.execute(input, data);
+        assertThat(action.execute(input, data), is(true));
     }
 }
