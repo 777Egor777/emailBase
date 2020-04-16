f684abbcb05211634222947b44acade439e7ffe2
Egor <yegeraskin13@gmail.com>
2020-04-16 21:27:41 +0400

    Add test class for class "ExitProgramAction"

---
+++ src/test/java/ru/job4j/action/ExitProgramActionTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.action;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Test class for class
+ * {@code ExitProgramAction}
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ */
+public class ExitProgramActionTest {
+
+    @Test
+    public void getTitle() {
+    }
+
+    @Test
+    public void execute() {
+    }
+}
