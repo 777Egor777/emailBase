36530d518ba2217990fe0822b2fc1910b2515945
Egor <yegeraskin13@gmail.com>
2020-04-16 23:48:33 +0400

    Add test class for class "CompressData"

---
+++ src/test/java/ru/job4j/action/CompressDataActionTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.action;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Test class for class
+ * {@code CompressDataAction}
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ */
+public class CompressDataActionTest {
+
+    @Test
+    public void getTitle() {
+    }
+
+    @Test
+    public void execute() {
+    }
+}
