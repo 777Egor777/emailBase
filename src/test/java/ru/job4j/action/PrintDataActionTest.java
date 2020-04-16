4305fc0f2a9b06a78ef6900122a967fda79653d2
Egor <yegeraskin13@gmail.com>
2020-04-16 17:54:52 +0400

    Tests method "execute" class "PrintDataActionTest"

--- src/test/java/ru/job4j/action/PrintDataActionTest.java
+++ src/test/java/ru/job4j/action/PrintDataActionTest.java
@@ -1,6 +1,13 @@
 package ru.job4j.action;
 
 import org.junit.Test;
+import ru.job4j.input.Input;
+import ru.job4j.model.database.Data;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.StringJoiner;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.is;
 
@@ -14,11 +21,34 @@ public class PrintDataActionTest {
 
     @Test
     public void getTitle() {
-        UserAction action = new StubAction();
+        UserAction action = new PrintDataAction();
         assertThat(action.getTitle(), is("PRINT"));
     }
 
     @Test
-    public void execute() {
+    public void execute1() {
+        UserAction action = new PrintDataAction();
+        Input input = null;
+        Data data = new Data();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        StringJoiner joiner = new StringJoiner(System.lineSeparator(), "", System.lineSeparator());
+        PrintStream stdout = System.out;
+        System.setOut(new PrintStream(baos));
+
+        action.execute(input, data);
+        joiner.add("===PRINT===");
+        joiner.add(data.toString());
+
+        System.setOut(stdout);
+        assertThat(baos.toString(), is(joiner.toString()));
+    }
+
+    @Test
+    public void execute2() {
+        UserAction action = new PrintDataAction();
+        Input input = null;
+        Data data = new Data();
+        action.execute(input, data);
+        assertThat(action.execute(input, data), is(true));
     }
 }
