3394a424e90e2aa1d2049211f2268ee929c99c98
Egor <yegeraskin13@gmail.com>
2020-04-16 23:04:11 +0400

    Add test class for class "DeleteUserAction"

---
+++ src/test/java/ru/job4j/action/DeleteUserActionTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.action;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Test class for class
+ * {@code DeleteUserAction}
+ * @author Geraskin Yegor(yegeraskin13@gmail.com)
+ */
+public class DeleteUserActionTest {
+
+    @Test
+    public void getTitle() {
+    }
+
+    @Test
+    public void execute() {
+    }
+}
