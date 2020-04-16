aae814ade5b158115ca75295a992c662722e297b
Egor <yegeraskin13@gmail.com>
2020-04-16 21:25:05 +0400

    Add tests for method "execute" class "OverwriteDataAc"

--- src/test/java/ru/job4j/action/OverwriteDataActionTest.java
+++ src/test/java/ru/job4j/action/OverwriteDataActionTest.java
@@ -1,6 +1,17 @@
 package ru.job4j.action;
 
 import org.junit.Test;
+import ru.job4j.input.Input;
+import ru.job4j.input.StubInput;
+import ru.job4j.input.ValidateInput;
+import ru.job4j.model.database.Data;
+import ru.job4j.model.user.User;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.StringJoiner;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.is;
 
@@ -13,11 +24,44 @@ public class OverwriteDataActionTest {
 
     @Test
     public void getTitle() {
-        UserAction action = new PrintDataAction();
-        assertThat(action.getTitle(), is("OVERWRITE"));
+        UserAction action = new OverwriteDataAction();
+        assertThat(action.getTitle(), is("OVERWRITE "));
     }
 
     @Test
-    public void execute() {
+    public void execute1() {
+        UserAction action = new OverwriteDataAction();
+        Input input = new ValidateInput(new StubInput(Arrays.asList(
+            "1",
+            "1",
+            "yegeraskin13@gmail.com"
+        )));
+        Data data = new Data();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        StringJoiner joiner = new StringJoiner(System.lineSeparator(), "", System.lineSeparator());
+        PrintStream stdout = System.out;
+        System.setOut(new PrintStream(baos));
+
+        action.execute(input, data);
+        joiner.add("===OVERWRITE===");
+        joiner.add("Enter user's count: ");
+        joiner.add("Enter user's(1) email's count: ");
+        User user = data.getUserList().get(0);
+        joiner.add("user#" + user.getId() +  " email#1: ");
+        System.setOut(stdout);
+        assertThat(baos.toString(), is(joiner.toString()));
+    }
+
+    @Test
+    public void execute2() {
+        UserAction action = new OverwriteDataAction();
+        Input input = new ValidateInput(new StubInput(Arrays.asList(
+                "1",
+                "1",
+                "yegeraskin13@gmail.com"
+        )));
+        Data data = new Data();
+        action.execute(input, data);
+        assertThat(action.execute(input, data), is(true));
     }
 }
