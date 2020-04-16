9948c676baffc17879739ad1c39a7a5bd1c961f1
Egor <yegeraskin13@gmail.com>
2020-04-16 23:20:37 +0400

    Add test for method "execute" of class "DeleteUserAction"

--- src/test/java/ru/job4j/action/DeleteUserActionTest.java
+++ src/test/java/ru/job4j/action/DeleteUserActionTest.java
@@ -1,6 +1,16 @@
 package ru.job4j.action;
 
 import org.junit.Test;
+import ru.job4j.input.Input;
+import ru.job4j.input.StubInput;
+import ru.job4j.input.ValidateInput;
+import ru.job4j.model.database.Data;
+import ru.job4j.model.user.User;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.StringJoiner;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.is;
@@ -19,6 +29,36 @@ public class DeleteUserActionTest {
     }
 
     @Test
-    public void execute() {
+    public void execute1() {
+        UserAction action = new DeleteUserAction();
+        Data data = new Data();
+        User user = new User();
+        data.addUser(user);
+        Input input = new ValidateInput(new StubInput(Arrays.asList(
+                ""+user.getId()
+        )));
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        StringJoiner joiner = new StringJoiner(System.lineSeparator(), "", System.lineSeparator());
+        PrintStream stdout = System.out;
+        System.setOut(new PrintStream(baos));
+
+        action.execute(input, data);
+        joiner.add("===DELETE USER===");
+        joiner.add("Enter user's id: ");
+
+        System.setOut(stdout);
+        assertThat(baos.toString(), is(joiner.toString()));
+    }
+
+    @Test
+    public void execute2() {
+        UserAction action = new DeleteUserAction();
+        Data data = new Data();
+        User user = new User();
+        data.addUser(user);
+        Input input = new ValidateInput(new StubInput(Arrays.asList(
+                ""+user.getId()
+        )));
+        assertThat(action.execute(input, data), is(true));
     }
 }
