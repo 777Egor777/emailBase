d605f75422e8ffdb84957054dffeb3e95f21a2fa
Egor <yegeraskin13@gmail.com>
2020-04-17 17:48:50 +0400

    Impl method "isCorrectValue" class "Emails"

--- src/main/java/ru/job4j/model/email/util/Emails.java
+++ src/main/java/ru/job4j/model/email/util/Emails.java
@@ -94,7 +94,7 @@ public class Emails {
      *                object
      */
     public static boolean isCorrectValue(String value) {
-        return false;
+        return isCorrectEmail(new Email(value));
     }
 
     /**
